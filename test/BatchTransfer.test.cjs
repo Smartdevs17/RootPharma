a741793ab42aa2ee93fb991c97033b2c6f3a026d
smartdev <smartdevs17@gmail.com>
2026-01-29 09:47:07 +0100

    test(RootPharma): fix and complete BatchTransfer unit tests

--- test/BatchTransfer.test.cjs
+++ test/BatchTransfer.test.cjs
@@ -1,6 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 
 describe("BatchTransfer", function () {
   async function deployFixture() {
@@ -31,10 +32,64 @@ describe("BatchTransfer", function () {
       expect(history[0].isReceived).to.be.false;
     });
 
-    it("Should fail if recipient is zero address", async function () {
-      const { batchTransfer, from } = await loadFixture(deployFixture);
-      await expect(batchTransfer.connect(from).initiateTransfer(1, ethers.ZeroAddress, "L", "N"))
-        .to.be.revertedWith("Invalid recipient");
+    it("Should fail if non-holder tries to initiate after first transfer", async function () {
+      const { batchTransfer, from, to, other } = await loadFixture(deployFixture);
+      
+      // from -> to
+      await batchTransfer.connect(from).initiateTransfer(1, to.address, "L", "N");
+      await batchTransfer.connect(to).confirmReceipt(1);
+      
+      // 'other' is NOT holder
+      await expect(batchTransfer.connect(other).initiateTransfer(1, from.address, "L", "N"))
+        .to.be.revertedWith("Not current holder");
     });
   });
-});function anyValue(arg) { return true; }
+
+  describe("Confirmation", function () {
+    it("Should confirm receipt successfully", async function () {
+      const { batchTransfer, from, to } = await loadFixture(deployFixture);
+      await batchTransfer.connect(from).initiateTransfer(1, to.address, "L", "N");
+      
+      await expect(batchTransfer.connect(to).confirmReceipt(1))
+        .to.emit(batchTransfer, "TransferReceived")
+        .withArgs(1, to.address);
+
+      expect(await batchTransfer.getCurrentHolder(1)).to.equal(to.address);
+      const history = await batchTransfer.getTransferHistory(1);
+      expect(history[0].isReceived).to.be.true;
+    });
+
+    it("Should fail if non-recipient tries to confirm", async function () {
+      const { batchTransfer, from, to, other } = await loadFixture(deployFixture);
+      await batchTransfer.connect(from).initiateTransfer(1, to.address, "L", "N");
+      
+      await expect(batchTransfer.connect(other).confirmReceipt(1))
+        .to.be.revertedWith("Not the recipient");
+    });
+
+    it("Should fail if no transfers found", async function () {
+      const { batchTransfer, to } = await loadFixture(deployFixture);
+      await expect(batchTransfer.connect(to).confirmReceipt(1))
+        .to.be.revertedWith("No transfers found");
+    });
+  });
+
+  describe("Ownership Flow", function () {
+    it("Should only allow the current holder to initiate next transfer", async function () {
+      const { batchTransfer, from, to, other } = await loadFixture(deployFixture);
+      
+      // from -> to
+      await batchTransfer.connect(from).initiateTransfer(1, to.address, "L", "N");
+      await batchTransfer.connect(to).confirmReceipt(1);
+      
+      // 'from' tries to initiate again
+      await expect(batchTransfer.connect(from).initiateTransfer(1, other.address, "L", "N"))
+        .to.be.revertedWith("Not current holder");
+        
+      // 'to' (current holder) initiates
+      await expect(batchTransfer.connect(to).initiateTransfer(1, other.address, "L2", "N2"))
+        .to.emit(batchTransfer, "TransferInitiated")
+        .withArgs(1, to.address, other.address, anyValue);
+    });
+  });
+});
